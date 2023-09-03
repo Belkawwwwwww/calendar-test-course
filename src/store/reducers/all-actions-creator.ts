b9449a6c472493a5f63e482d565704a00721401c
Belkawwwwwww <89457855+Belkawwwwwww@users.noreply.github.com>
2023-09-03 19:23:31 +0500

    Update all-actions-creator.ts

--- src/store/reducers/all-actions-creator.ts
+++ src/store/reducers/all-actions-creator.ts
@@ -1,8 +1,6 @@
 import { AuthActionCreators } from './auth/action-creators';
 import {EventActionCreators} from "./event/action-creators";
 
-//Отсюда будем экспортировать просто некоторый обожающий объект и в этот объект будем разворачивать
-// уже те экшн креаторы, которые на данный момент уже созданы
 export const allActionCreator = {
     ...AuthActionCreators,
     ...EventActionCreators
