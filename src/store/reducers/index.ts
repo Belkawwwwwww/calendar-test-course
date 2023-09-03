11d06cbb51a845957fd9783b112d69f66fdb516f
Belkawwwwwww <89457855+Belkawwwwwww@users.noreply.github.com>
2023-09-03 19:23:46 +0500

    Update index.ts

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,5 +1,3 @@
-//все редюсеры которые будут в приложении будем импортировать сюда
-// и одной пачкой от сюда экспортировать
 import auth from './auth'
 import event from './event';
 export default {
