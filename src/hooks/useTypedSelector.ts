473d743a33ce98b538a5587149c3be16f1d260ca
Belkawwwwwww <89457855+Belkawwwwwww@users.noreply.github.com>
2023-09-03 19:22:07 +0500

    Update useTypedSelector.ts

--- src/hooks/useTypedSelector.ts
+++ src/hooks/useTypedSelector.ts
@@ -1,11 +1,7 @@
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import {AppDispatch, RootState} from '../store'
 
-//экспортируем сюда константу и сразу задаем ей тип
-//это спрециальный тип который предоставляет редакс
-//в качесвте дженерика указываем тот тип который отвечает за состояние приложения из /store/index.ts - RootState
-//и приравниваем его к UseState
-//
+
 
 /*
 export const useAppDispatch = () => useDispatch<AppDispatch>()
